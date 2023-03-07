47be02099a7798d72acf0b2d9142ad2e2369f347
shahinahmed6057 <mdshahinahmed605701@gmail.com>
2023-03-07 14:59:56 +0600

    Completed routing

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,21 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
 import React from "react";
 
 const Home = () => {
-  return <div>Home Page</div>;
+  const router = useRouter();
+  const handleClick = () => {
+    console.log("Placing your order");
+    router.push("/product");
+  };
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <Link href="blog">Blog</Link>
+      <Link href="product">Products</Link>
+      <button onClick={handleClick}>Please Order</button>
+    </div>
+  );
 };
 
 export default Home;
--- pages/product/index.js
+++ pages/product/index.js
@@ -1,9 +1,20 @@
+import Link from "next/link";
+
 const ProductList = () => {
   return (
     <div>
-      <h2>Product 1</h2>
-      <h2>Product 2</h2>
-      <h2>Product 3</h2>
+      <Link href="/">Home</Link>
+      <h2>
+        <Link href="product/1">Product 1</Link>
+      </h2>
+      <h2>
+        <Link href="product/2" replace>
+          Product 2
+        </Link>
+      </h2>
+      <h2>
+        <Link href="product/3">Product 3</Link>
+      </h2>
     </div>
   );
 };
